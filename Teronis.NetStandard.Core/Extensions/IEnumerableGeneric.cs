6a9c5966c531a0097b362e90228d4da861f7948d
teroneko <giteroneko@gmail.com>
2019-04-01 15:16:56 +0100

    Intermediate commit: Refactor method

--- Teronis.NetStandard.Core/Extensions/IEnumerableGeneric.cs
+++ Teronis.NetStandard.Core/Extensions/IEnumerableGeneric.cs
@@ -138,7 +138,123 @@ namespace Teronis.NetStandard.Extensions
                     leftValueIndexPairs.RemoveAt(leftValueIndex);
                 }
 
+                foreach (var rightValueIndexPair in rightValueIndexPairs)
+                    yield return new CollectionChange<T>(NotifyCollectionChangedAction.Add, rightValueIndexPair.Value, rightValueIndexPair.Index);
+            }
+        }
 
+        /// <summary>
+        /// update -> remove -> add
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="leftValues"></param>
+        /// <param name="rightValues"></param>
+        /// <param name="equalityComparer"></param>
+        /// <returns></returns>
+        public static IEnumerable<CollectionChange<T>> GetCollectionDifferences2<T>(this IEnumerable<T> leftValues, IEnumerable<T> rightValues, IEqualityComparer<T> equalityComparer)
+        {
+            var leftValuesEnumerator = leftValues.GetEnumerator();
+            var rightValuesEnumerator = rightValues.GetEnumerator();
+            //bool hasLeftValue, hasRightValue;
+            var leftValueIndex = 0;
+            var rightValueIndex = 0;
+            var leftValueIndexPairs = new List<ValueIndexPair<T>>();
+            var rightValueIndexPairs = new List<ValueIndexPair<T>>();
+            //var isFirstIterationSynced = true;
+            var areEarlyInterationValuesEqual = true;
+
+            do {
+                var hasLeftValue = leftValuesEnumerator.MoveNext();
+                var hasRightValue = rightValuesEnumerator.MoveNext();
+
+                // Cancel if not a left or right value is upcoming
+                if (!(hasLeftValue | hasRightValue))
+                    break;
+
+                var leftValue = leftValuesEnumerator.Current;
+                var rightValue = rightValuesEnumerator.Current;
+
+                if (hasLeftValue && hasRightValue) {
+                    if (equalityComparer.Equals(leftValue, rightValue))
+                        // Changes on the same index can be returned immediately
+                        yield return new CollectionChange<T>(NotifyCollectionChangedAction.Replace, leftValue, leftValueIndex, rightValue, rightValueIndex);
+                    else {
+                        leftValueIndexPairs.Add(new ValueIndexPair<T>(leftValue, leftValueIndex));
+                        rightValueIndexPairs.Add(new ValueIndexPair<T>(rightValue, rightValueIndex));
+                        areEarlyInterationValuesEqual = false;
+                    }
+
+                    leftValueIndex++;
+                    rightValueIndex++;
+                }
+            } while (true);
+
+            while ((hasLeftValue = leftValuesEnumerator.MoveNext()) | (hasRightValue = rightValuesEnumerator.MoveNext())) {
+                var leftValue = leftValuesEnumerator.Current;
+                var rightValue = rightValuesEnumerator.Current;
+
+                if (hasLeftValue && hasRightValue) {
+                    if (equalityComparer.Equals(leftValue, rightValue))
+                        yield return new CollectionChange<T>(NotifyCollectionChangedAction.Replace, leftValue, leftValueIndex, rightValue, rightValueIndex);
+                    else {
+                        leftValueIndexPairs.Add(new ValueIndexPair<T>(leftValue, leftValueIndex));
+                        rightValueIndexPairs.Add(new ValueIndexPair<T>(rightValue, rightValueIndex));
+                        isFirstIterationSynced = false;
+                    }
+
+                    leftValueIndex++;
+                    rightValueIndex++;
+                } else if (hasLeftValue) {
+                    if (isFirstIterationSynced)
+                        leftValueIndexPairs.Add(new ValueIndexPair<T>(leftValue, leftValueIndex));
+                    else
+                        yield return new CollectionChange<T>(NotifyCollectionChangedAction.Remove, leftValue, leftValueIndex);
+                } else {
+                    if (isFirstIterationSynced)
+                        rightValueIndexPairs.Add(new ValueIndexPair<T>(rightValue, rightValueIndex));
+                    else
+                        yield return new CollectionChange<T>(NotifyCollectionChangedAction.Add, rightValue, rightValueIndex);
+
+                    rightValueIndex++;
+                }
+            }
+
+            if (isFirstIterationSynced)
+                yield break;
+
+            if (leftValueIndexPairs.Count == 0) {
+                foreach (var rightValueIndexPair in rightValueIndexPairs)
+                    yield return new CollectionChange<T>(NotifyCollectionChangedAction.Add, rightValueIndexPair.Value, rightValueIndexPair.Index);
+            } else if (rightValueIndexPairs.Count == 0) {
+                foreach (var leftValueIndexPair in leftValueIndexPairs)
+                    yield return new CollectionChange<T>(NotifyCollectionChangedAction.Remove, leftValueIndexPair.Value, leftValueIndexPair.Index);
+            } else {
+                var leftValueIndexPairsCount = leftValueIndexPairs.Count;
+                var rightValueIndexPairsCount = rightValueIndexPairs.Count;
+                var ttbRemovedValues = new List<CollectionChange<T>>();
+                var ttbMovedValues = new List<CollectionChange<T>>();
+                var ttbAddedValues = new List<ValueIndexPair<T>>();
+
+                for (leftValueIndex = leftValueIndexPairs.Count - 1; leftValueIndex >= 0; leftValueIndex--) {
+                    var leftValueIndexPair = leftValueIndexPairs[leftValueIndex];
+                    ValueIndexPair<ValueIndexPair<T>>? foundRightValueIndexPair = null;
+
+                    for (rightValueIndex = rightValueIndexPairs.Count - 1; rightValueIndex >= 0; rightValueIndex--) {
+                        var rightValueIndexPair = rightValueIndexPairs[rightValueIndex];
+
+                        if (equalityComparer.Equals(leftValueIndexPair))
+                            foundRightValueIndexPair = new ValueIndexPair<ValueIndexPair<T>>(rightValueIndexPair, rightValueIndex);
+                    }
+
+                    if (foundRightValueIndexPair == null) {
+                        ttbRemovedValues.Add(new CollectionChange<T>(NotifyCollectionChangedAction.Remove, leftValueIndexPair.Value, leftValueIndexPair.Index));
+                    } else {
+                        ttbMovedValues.Add(new CollectionChange<T>(NotifyCollectionChangedAction.Move, leftValueIndexPair.Value, leftValueIndexPair.Index, foundRightValueIndexPair.Value.Value.Value, foundRightValueIndexPair.Value.Value.Index));
+                        rightValueIndexPairs.RemoveAt(foundRightValueIndexPair.Value.Index);
+                    }
+
+                    leftValueIndexPairs.RemoveAt(leftValueIndex);
+                }
 
                 foreach (var rightValueIndexPair in rightValueIndexPairs)
                     yield return new CollectionChange<T>(NotifyCollectionChangedAction.Add, rightValueIndexPair.Value, rightValueIndexPair.Index);

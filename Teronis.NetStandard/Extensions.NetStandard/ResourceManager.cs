6e55eee2e22d27e81ca443fdc4742cbb7bb8c65e
teroneko <giteroneko@gmail.com>
2019-04-12 15:32:04 +0100

    Several tool classes added

---
+++ Teronis.NetStandard/Extensions.NetStandard/ResourceManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Resources;
+using System.Text;
+using Teronis.Tools.NetStandard;
+
+namespace Teronis.Extensions.NetStandard
+{
+    public static class ResourceManagerExtensions
+    {
+        public static List<CultureInfo> GetSupportedCultures(this ResourceManager resourceManager)
+            => ResourceManagerTools.GetSupportedCultures(resourceManager);
+    }
+}

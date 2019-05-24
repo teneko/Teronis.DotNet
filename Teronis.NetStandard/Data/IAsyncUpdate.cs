a8adf53cc9426eadee5919de4f4fe8eb629e6870
teroneko <giteroneko@gmail.com>
2019-05-24 15:19:13 +0100

    Tool- and extension-functions added

---
+++ Teronis.NetStandard/Data/IAsyncUpdate.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace Teronis.Data
+{
+    public interface IAsyncUpdate<T> : IUpdatable<T>
+    {
+        Task UpdateByAsync(T content);
+    }
+}
--- Teronis.NetStandard/Tools.NetStandard/Generic.cs
+++ Teronis.NetStandard/Tools.NetStandard/Generic.cs
@@ -1,9 +1,12 @@
 ﻿using System;
+using System.Linq;
 using System.Threading.Tasks;
+using Teronis.Reflection;
+using Teronis.Extensions.NetStandard;
 
 namespace Teronis.Tools.NetStandard
 {
-    public class GenericTools
+    public static class GenericTools
     {
         public static bool ReturnNonDefault<T>(T inValue, out T outValue, Func<T> getNonDefaultIfDefault = null)
             => !Tools.CompareEquality(outValue = inValue, default) || (FuncGenericTools.ReturnIsInvocable(getNonDefaultIfDefault, out outValue) && !Tools.CompareEquality(outValue, default));
@@ -34,7 +37,7 @@ namespace Teronis.Tools.NetStandard
             await task;
             return inValue;
         }
-        
+
         public static V ReturnValue<I, V>(I inValue, out I outInValue, V value)
         {
             outInValue = inValue;
@@ -55,5 +58,45 @@ namespace Teronis.Tools.NetStandard
 
         public static V ReturnValue<I, V>(I inValue, Func<I, V> getValue)
             => getValue(inValue);
+        
+        public static TCloningObjectType ShallowCopy<TCloningObjectType, TCopyingObject>(TCloningObjectType cloningObject, TCopyingObject copyingObject)
+        {
+            var cloningObjectVariableInfoSettings = new VariableInfoSettings() {
+                IncludeIfWritable = true,
+            };
+
+            var clonedType = typeof(TCloningObjectType);
+
+            var cloningObjectVariableInfoByNameList = clonedType
+                .GetVariableInfos(cloningObjectVariableInfoSettings)
+                .ToDictionary(x => x.Name);
+
+            var copyingObjectVariableInfoSettings = new VariableInfoSettings() {
+                IncludeIfReadable = true,
+            };
+
+            var copyingObjectVariableInfoByNameList = typeof(TCopyingObject)
+                .GetVariableInfos(copyingObjectVariableInfoSettings)
+                .ToDictionary(x => x.Name);
+
+            var serializerSettings = (TCloningObjectType)clonedType.InstantiateUninitializedObject();
+
+            foreach (var nameAndCloningObjectVariableInfoPair in cloningObjectVariableInfoByNameList) {
+                var cloningObjectVariableInfoKey = nameAndCloningObjectVariableInfoPair.Key;
+
+                if (copyingObjectVariableInfoByNameList.ContainsKey(cloningObjectVariableInfoKey)) {
+                    var cloningObjectVariableInfo = nameAndCloningObjectVariableInfoPair.Value;
+                    var copyingObjectVariableInfo = copyingObjectVariableInfoByNameList[cloningObjectVariableInfoKey];
+                    var copyingObjectVariableValue = copyingObjectVariableInfo.GetValue(copyingObject);
+
+                    cloningObjectVariableInfo.SetValue(serializerSettings, copyingObjectVariableValue);
+                }
+            }
+
+            return serializerSettings;
+        }
+
+        public static TCloningObjectType ShallowCopy<TCloningObjectType>(TCloningObjectType cloningObject)
+            => ShallowCopy(cloningObject, cloningObject);
     }
 }

b08e64f4b5b83c2bc5b85fb4e026b6a42af0f4d9
teroneko <giteroneko@gmail.com>
2019-06-12 11:17:50 +0200

    Function Bubblesort implemented

--- Teronis.NetStandard/Tools.NetStandard/IList.cs
+++ Teronis.NetStandard/Tools.NetStandard/IList.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
 
 namespace Teronis.Tools.NetStandard
 {
@@ -10,7 +9,8 @@ namespace Teronis.Tools.NetStandard
         {
             int n = list.Count;
 
-            while (n > 1) {
+            while (n > 1)
+            {
                 n--;
                 int k = ThreadSafeRandom.Next(n + 1);
                 var value = list[k];
@@ -18,5 +18,41 @@ namespace Teronis.Tools.NetStandard
                 list[n] = value;
             }
         }
+
+        public static void Bubblesort<T>(IList<T> collection, IComparer<T> comparer)
+        {
+            // When null, throw exception
+            if (collection == null)
+                throw new ArgumentNullException(nameof(collection));
+
+            // There have to be at least two items to sort them
+            if (collection.Count < 2)
+                return;
+
+            // -1 for zero based index and another 
+            // -1 for beginning at the second last index
+            var currentIndex = collection.Count - 2;
+
+            while (currentIndex >= 0)
+            {
+                for (var index = 0; index <= currentIndex; index++)
+                {
+                    var foreNumber = collection[index];
+                    var backIndex = index + 1;
+                    var backNumber = collection[backIndex];
+
+                    if (comparer.Compare(foreNumber, backNumber) > 0)
+                    {
+                        collection[backIndex] = foreNumber;
+                        collection[index] = backNumber;
+                    }
+                }
+
+                currentIndex--;
+            }
+        }
+
+        public static void Bubblesort<T>(IList<T> collection)
+            => Bubblesort(collection, Comparer<T>.Default);
     }
 }

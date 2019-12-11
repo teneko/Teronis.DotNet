abe1ba2e74a936c113c921ef7ccd4dbfd6d8f1a3
teroneko <giteroneko@gmail.com>
2019-12-11 08:33:43 +0000

    Extension class and file IUpdateExtensions(.cs) renamed to IContentUpdateExtensions(.cs); Keyword default usage corrected in IEnumerable{T}Extensions.cs;

--- Teronis.NetStandard/Extensions.NetStandard/IContentUpdateExtensions.cs
+++ Teronis.NetStandard/Extensions.NetStandard/IContentUpdateExtensions.cs
@@ -3,7 +3,7 @@ using Teronis.Data;
 
 namespace Teronis.Extensions.NetStandard
 {
-    public static class UpdateExtensions
+    public static class IContentUpdateExtensions
     {
         public static ContentUpdate<InnerContentType> CreateUpdateFromContent<ContentType, InnerContentType>(this IContentUpdate<ContentType> update, Func<ContentType, InnerContentType> getInnerContent, object updateCreationSource)
             => new ContentUpdate<InnerContentType>(getInnerContent(update.Content), update.OriginalUpdateCreationSource, updateCreationSource);

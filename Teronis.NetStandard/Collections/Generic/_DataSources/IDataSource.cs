92fdd22025d13db8d1b1d7d7453737dde64a0d6d
teroneko <giteroneko@gmail.com>
2019-12-11 08:04:11 +0000

    Package Microsoft.Extensions.Logging.Abstractions added

---
+++ Teronis.NetStandard/Collections/Generic/_DataSources/IDataSource.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Teronis.Collections.Generic
+{
+    public interface IDataSource : IDisposable
+    {
+        DataSourceEnumerationState EnumerationState { get; }
+
+        /// <summary>
+        /// Type of data this data source delivers.
+        /// </summary>
+        Type DataType { get; }
+
+        public bool IsDisposed { get; }
+    }
+}

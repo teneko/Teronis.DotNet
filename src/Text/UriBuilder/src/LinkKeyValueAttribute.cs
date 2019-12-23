d6d76b931a16219dbd85ec61e207d03791365e9a
teroneko <giteroneko@gmail.com>
2019-12-23 07:18:54 +0000

    Source code from Teronis.NetStandard.UriBuilder are now included as Teronis.Text.UriBuilder

---
+++ src/Text/UriBuilder/src/LinkKeyValueAttribute.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Teronis.Text.UriBuilder
+{
+    public class LinkKeyValueAttribute : Attribute
+    {
+        public object DefaultValue {
+            get { return @default; }
+            set {
+                @default = value;
+                IsDefaultSet = true;
+            }
+        }
+        public bool IsDefaultSet { get; private set; }
+
+        public string Name;
+
+        object @default;
+
+        public bool ProofValueEquality(object val)
+        {
+            if (@default == null && val == null)
+                return true;
+            else if (@default == null || val == null)
+                return false;
+            else
+                return DefaultValue.Equals(val);
+        }
+
+        public LinkKeyValueAttribute() { }
+
+        public LinkKeyValueAttribute(bool nullable)
+        {
+            IsDefaultSet = nullable;
+        }
+    }
+}
---
+++ src/Text/UriBuilder/src/LinkPathValueAttribute.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Teronis.Text.UriBuilder
+{
+    public class LinkPathValueAttribute : Attribute
+    {
+        public string Name;
+    }
+}

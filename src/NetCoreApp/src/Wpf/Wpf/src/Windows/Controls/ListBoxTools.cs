8839fc4e4c3db631d97d339259b77825111985c1
teroneko <giteroneko@gmail.com>
2020-01-08 15:18:54 +0000

    Visual tree helper code added

---
+++ src/NetCoreApp/src/Wpf/Wpf/src/Windows/Controls/ListBoxTools.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+
+namespace Teronis.Windows.Controls
+{
+    public static class ListBoxTools
+    {
+        private static ListViewItem getListViewItem(ListBox listBox, int index)
+        {
+            if (listBox.ItemContainerGenerator.Status != GeneratorStatus.ContainersGenerated)
+                return null;
+
+            return listBox.ItemContainerGenerator.ContainerFromIndex(index) as ListViewItem;
+        }
+
+        public static int GetSelectedItemIndexAtPosition(ListBox listBox, GetPositionFromInputElementDelegate getPosition)
+        {
+            var index = -1;
+
+            if (listBox?.Items != null) {
+                for (int i = 0; i < listBox.Items.Count; ++i) {
+                    if (listBox.SelectedItems.IndexOf(listBox.Items[i]) == -1)
+                        continue;
+
+                    var listViewItem = getListViewItem(listBox, i);
+
+                    if (VisualTools.IsMouseOverTarget(listViewItem, getPosition)) {
+                        index = i;
+                        break;
+                    }
+                }
+            }
+
+            return index;
+        }
+    }
+}
---
+++ src/NetCoreApp/src/Wpf/Wpf/src/Windows/Controls/VisualTools.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+using System.Windows.Media;
+
+namespace Teronis.Windows.Controls
+{
+    public class VisualTools
+    {
+        public static bool IsMouseOverTarget(Visual target, GetPositionFromInputElementDelegate getPosition)
+        {
+            // It can happen that target is null
+            if (target == null)
+                return false;
+
+            var bounds = VisualTreeHelper.GetDescendantBounds(target);
+            var mousePos = getPosition((IInputElement)target);
+            return bounds.Contains(mousePos);
+        }
+    }
+}

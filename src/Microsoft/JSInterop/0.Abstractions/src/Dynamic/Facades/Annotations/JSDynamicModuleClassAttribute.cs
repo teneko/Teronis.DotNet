42a5f0ff382ff46f42d9310175d8bfd75225dbcd
teroneko <giteroneko@gmail.com>
2021-03-20 06:50:07 +0100

    Project paths fixed and old projects deleted

--- src/Microsoft/JSInterop/0.Abstractions/src/Dynamic/Facades/Annotations/JSDynamicModuleClassAttribute.cs
+++ src/Microsoft/JSInterop/0.Abstractions/src/Dynamic/Facades/Annotations/JSDynamicModuleClassAttribute.cs
@@ -1,7 +1,16 @@
-﻿using Teronis.Microsoft.JSInterop.Facades.Annotations;
+﻿using System;
+using Teronis.Microsoft.JSInterop.Facades.Annotations;
 
 namespace Teronis.Microsoft.JSInterop.Dynamic.Facades.Annotations
 {
+    /// <summary>
+    /// Decoratable on interface. It provides
+    /// <see cref="JSModuleAttributeBase.ModuleNameOrPath"/>
+    /// to those properties with facade attribute that are using
+    /// parameterless constructor but not
+    /// <see cref="JSDynamicModulePropertyAttribute(string)"/>.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Interface, Inherited = true)]
     public class JSDynamicModuleClassAttribute : JSModuleClassAttribute
     {
         public JSDynamicModuleClassAttribute(string ModuleNameOrPath)

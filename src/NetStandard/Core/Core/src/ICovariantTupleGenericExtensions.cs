f2877bf45d1af3b4c6b47a2d8d84b43f9ab63722
teroneko <giteroneko@gmail.com>
2021-04-09 21:29:54 +0100

    Filenames adapts now interface names - Project Teronis.Nullable is now added as reference too in netcoreapp3.0 and netcoreapp3.1

--- src/NetStandard/Core/Core/src/ICovariantTupleGenericExtensions.cs
+++ src/NetStandard/Core/Core/src/ICovariantTupleGenericExtensions.cs
@@ -3,7 +3,7 @@
 
 namespace Teronis
 {
-    public static class ICovariantTupleExtensions
+    public static class ICovariantTupleGenericExtensions
     {
         public static void Deconstruct<T1, T2>(this ICovariantTuple<T1, T2> tuple, out T1 item1, out T2 item2) {
             item1 = tuple.Item1;
